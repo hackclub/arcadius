f1dc8a354adb2a466afd413234aa1d742e645ba1
Christopher Walker <15720465+polytroper@users.noreply.github.com>
2024-06-17 10:37:20 -0400

    Revert "fix chris's stupid error -_-"
    This reverts commit 9d20498db61ad044861d20451bfe56a97475532c.

--- src/index.ts
+++ src/index.ts
@@ -130,7 +130,7 @@ receiver.router.post(
       console.log(`Creating arcade user for ${userId}`);
       const user = (await client.users.info({ user: userId })).user;
       try {
-        let triggeredBy: flowTriggeredByEnum = flowTriggeredByEnum.arcadius;
+        let triggeredBy: flowTriggeredByEnum = flowTriggeredByEnum.hedi;
 
         const arcadeUser = await createArcadeUser(
           userId,
